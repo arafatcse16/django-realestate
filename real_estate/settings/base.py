6d2216a36c8fe2751cfd1a1f2b2090661bac96ed
arafat <arafatcse16@gmail.com>
2025-05-02 12:53:59 +0600

    add customer user model

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -137,6 +137,8 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.User"
+
 
 import logging
 import logging.config
--- real_estate/urls.py
+++ real_estate/urls.py
@@ -7,3 +7,14 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("supersecret/", admin.site.urls),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Real Estate Admin"
+admin.site.site_title = "Real Estate Admin Portal"
+admin.site.index_title = "Welcome to Real Estate Admin Portal"
+# urlpatterns += [
+#     path("api/v1/users/", include("apps.users.urls")),
+#     path("api/v1/real-estate/", include("apps.real_estate.urls")),
+#     path("api/v1/locations/", include("apps.locations.urls")),
+#     path("api/v1/transactions/", include("apps.transactions.urls")),
+#     path("api/v1/notifications/", include("apps.notifications.urls")),
+# ]
