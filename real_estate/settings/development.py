e712e2adbd8c54b9a4029fe4b55baedbb6e0cf82
arafat <arafatcse16@gmail.com>
2025-05-04 12:12:41 +0600

    add users smpt server

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -6,6 +6,15 @@ from .base import *
 #         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+DOMAIN = env("DOMAIN")
+SITE_NAME="REAL ESTATE"
 
 DATABASES={
     "default": {
